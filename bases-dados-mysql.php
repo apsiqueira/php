98e60d69aa197f006cda6ff622e78fb5cc4a3029
Pablo-Jf <apsiqueira23@gmail.com>
2019-11-08 19:26:42 -0200

    Salvar e Bucar listando

--- bases-dados-mysql.php
+++ bases-dados-mysql.php
@@ -1,6 +1,7 @@
 	<?php
 
-use PhpParser\Node\Stmt\Echo_;
+	use PhpParser\Node\Stmt\Echo_;
+use Symfony\Component\Console\Helper\Table;
 
 include 'header.php';
 	include 'functions/functions.php';
@@ -27,62 +28,57 @@ include 'header.php';
 
 
 		$db_conection = new mysqli($server, $user, $password, $dbname, $port);
-   
-		if($db_conection->connect_error==true){
-			 echo "fail in conection";
-		}
-		else{
-			 echo "Conected is suscefull";
-			 echo'<br>';
-			 
-		 
-			 $sql =" INSERT INTO cliente (nomeCliente,email) values('Rogerio Primata', 'auior@gmail.com')" ;
 
-			 if($db_conection->query($sql)==true){
+		if ($db_conection->connect_error == true) {
+			echo "fail in conection";
+		} else {
+			echo "Conected is suscefull";
+			echo '<br>';
 
-				echo "Dados inseridos com Sucesso".'<br>';
-			 }
-			 else {
-				echo "Dados não form inseridos no banco".'<br>';
-			 };
-			 
-			 $selectAllClientes="call selectAllClientes";
 
-			//  $retorno= $db_conection->query($selectAllClientes);
+			//  $sql =" INSERT INTO cliente (nomeCliente,email) values('Rogerio Primata', 'auior@gmail.com')" ;
+
+			//  if($db_conection->query($sql)==true){
+
+			// 	echo "Dados inseridos com Sucesso".'<br>';
+			//  }
+			//  else {
+			// 	echo "Dados não form inseridos no banco".'<br>';
+			//  };
+
+			$selectAllClientes = "call selectAllClientes";
+
+			$result = $db_conection->query($selectAllClientes);?>
+			<table>
+				<tr>
+                    <th>Nome</th>
+					<th>Email</th>
+			
+
+				</tr>
+				
+
+		<?php	while($line= $result->fetch_assoc()){?>
+				
+			<tr>
+                  <td><?php echo $line["nomeCliente"]; ?> </td>
+                  <td> <?php echo $line ["email"]; ?> </td>
+
+			</tr>
+				
+
+
+		<?php 	}?>
+			</table>
+
+			
+
+
 		
-            
-			//  print_r($retorno); 
-
-			 
-		}
-
-       
-
-		?>
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-		<?php include 'functions/bottom_index.php'; ?>
+		<?php } ?>
+  	<?php include 'functions/bottom_index.php'; ?>
 
 
 	</body>
--- valida-formularios.php
+++ valida-formularios.php
@@ -1,61 +1,96 @@
-	<?php
+			<?php
 
-use PharIo\Manifest\Email;
+			use PharIo\Manifest\Email;
 
-include 'header.php';
-	$aula_atual = 'valida-formularios';
-	?>
-
-
-
-<?php 
-function clean_input($data){
-//retira espaço em branco
-$data_cleared=trim($data);
-//retira barras invertidas
-$data_cleared=stripslashes($data_cleared);
-//retira caracteres especiais 
-$data_cleared=htmlspecialchars($data_cleared);
-
-return $data_cleared;
-
-}
-?>
-
-	<body>
+			include 'header.php';
+			$aula_atual = 'valida-formularios';
+			?>
 
 
-		<h2>Formulários</h2>
-		<hr>
-		<small>Curso de Básico de PHP - Prof. Ivan Lourenço Gomes</small>
 
-		<h3>Envie seus dados</h3>
-		<pre>
+			<?php
+			function clean_input($data)
+			{
+				//retira espaço em branco
+				$data_cleared = trim($data);
+				//retira barras invertidas
+				$data_cleared = stripslashes($data_cleared);
+				//retira caracteres especiais 
+				$data_cleared = htmlspecialchars($data_cleared);
+
+				return $data_cleared;
+			}
+			?>
+
+			<body>
+
+
+				<h2>Formulários</h2>
+				<hr>
+				<small>Curso de Básico de PHP - Prof. Ivan Lourenço Gomes</small>
+
+				<h3>Envie seus dados</h3>
+				<pre>
 	   <?php
 		$erro_nome = null;
 		$erro_email = null;
+		$nome = null;
+		$msq_envio=null;
+		$email=null;
 
-			if ($_SERVER["REQUEST_METHOD"] == "POST") {
-				
-				echo "recebido via post";
-				$nome = $_POST["nome"];
-				$email = $_POST["email"];
+		if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
-				if ($nome == "") {
-					$erro_nome = "O nome e obrigatorio";
-				} elseif ($email == "") {
-					$erro_email = "Dados de email obrigatorio";
+			echo "recebido via post";
+			$nome = $_POST["nome"];
+			$email = $_POST["email"];
+
+			if ($nome == "") {
+				$erro_nome = "O nome e obrigatorio";
+			} elseif ($email == "") {
+				$erro_email = "Dados de email obrigatorio";
+			} elseif (filter_var($email, FILTER_VALIDATE_EMAIL) == false) {
+				$erro_email = "Email e invalido";
+			} else {
+				$nome = clean_input($nome);
+				$email = clean_input($email);
+
+
+				$server = "127.0.0.1";
+				$user = "root";
+				$password = "5122";
+				$dbname = "bd_udemi";
+				$port = 3306;
+
+
+				$db_conection = new mysqli($server, $user, $password, $dbname, $port);
+
+				if ($db_conection->connect_error == true) {
+					$msq_envio ="Falha no envio do ducumento";
+				} else {
+					$sql = " INSERT INTO cliente (nomeCliente,email) values('{$nome}', '{$email}')";
+
+					if ($db_conection->query($sql) == true) {
+
+
+						$msq_envio ="Envio do ducumento efeutado com sucesso";
+						$nome=NULL;
+						$email=NULL;
+						
+					} else {
+						$msq_envio ="Falha no envio do ducumento";
+					};
+
+					//  $selectAllClientes="call selectAllClientes";
+
+					//  $retorno= $db_conection->query($selectAllClientes);
+
+
+					//  print_r($retorno); 
+
+
 				}
-				elseif(filter_var($email,FILTER_VALIDATE_EMAIL)==false) {
-					$erro_email = "Email e invalido";
-
-
-				}else { 
-					$nome=clean_input($nome);
-					$email=clean_input($email);			
-				
-				}
-			} 
+			}
+		}
 
 		// print_r($_SERVER["REQUEST_METHOD"]);
 
@@ -68,40 +103,43 @@ return $data_cleared;
 
 
 
-		<form action="valida-formularios.php" method="post">
+				<form action="valida-formularios.php" method="post">
 
-			Nome: *
-			<br>
-			<input type="text" name="nome" class="field">
-			<br>
-			<div class="erro-form">
-				<?php echo $erro_nome;
-				?>
+					Nome: *
+					<br>
+					<input type="text" name="nome" class="field" value="<?php echo $nome ;?>">
+					<br>
+					<div class="erro-form">
+						<?php echo $erro_nome;
+						?>
 
-			</div>
-			<br>
+					</div>
+					<br>
 
-			E-mail: *
-			<br>
-			<input type="email" name="email" class="field">
-			<br>
-			<div class="erro-form">
-				<?php echo $erro_email
-				?>
+					E-mail: *
+					<br>
+					<input type="email" name="email" class="field" value="<?php echo $email; ?>">
+					<br>
+					<div class="erro-form">
+						<?php echo $erro_email
+						?>
 
-			</div>
+					</div>
 
 
-			<br>
+					<br>
+					
 
-			<input type="submit" name="submit" class="submit"><br>
-			<div class="sucesso-form">
+					<input type="submit" name="submit" class="submit"><br>
+					<div class="sucesso-form">
+					
 
-			</div>
+					</div>
+					<h3><?php echo $msq_envio;?></h3>
 
-		</form>
+				</form>
 
-		<pre>
+				<pre>
 
 		 <?php
 			echo "<p>Peguei o post </p>";
@@ -115,35 +153,24 @@ return $data_cleared;
 		</pre>
 
 
-		<h3>Confirmaçao</h3>
-		<p>Obrigado!</p>
-		<?php 
-		echo $nome;
-		echo $email;
-		
-		?>
+				<h3>Confirmaçao</h3>
+
+				<p>Obrigado : </p><br>
+			    <p>Status : <?php if($msq_envio!==null){
+
+					echo $msq_envio;
+				} ?></p>
+
+				
 
 
 
 
 
 
+				<?php include 'functions/bottom_index.php'; ?>
 
 
+			</body>
 
-
-
-
-
-
-
-
-
-
-
-		<?php include 'functions/bottom_index.php'; ?>
-
-
-	</body>
-
-	</html>
+			</html>
